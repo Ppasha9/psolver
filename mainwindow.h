a2c7f1bfcaa9cd0f916076dd5486791d48ddcde9
Ppasha9 <pavelvan50@gmail.com>
2019-06-15 20:15:32 +0300

    Add controller interface

--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,23 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    void addConnection();
+
 private:
     Ui::MainWindow *ui;
+
+    QSqlError addConnection(const QString &driver, const QString &dbName, const QString &host,
+                            const QString &user, const QString &passwd, int port = -1);
+
+    void loadSolver(const QString &path);
+    void loadProblem(const QString &path);
+
+    void solve();
+
+private slots:
+    void onSolve();
+    void onChooseTask(const QString &path);
+    void onAddSolver();
 };
 
 #endif // MAINWINDOW_H
